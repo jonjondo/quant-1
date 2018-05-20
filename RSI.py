42fe4b568663a89969eaee7f13477d63aea47afd
wangpeng <wangpenghehe@qq.com>
2018-05-20 23:45:17 +0800

    主要是更新了计算KDJ和MACD的代码，同时对于沪深MSCI的成份股进行KDJ金叉筛选。 下个版本计划先搞定均线的，再说，否则趋势是不对的。

--- RSI.py
+++ RSI.py
@@ -9,3 +9,14 @@ close = [float(x) for x in df['close']]
 df['RSI']=talib.RSI(np.array(close), timeperiod=12)     #RSI的天数一般是6、12、24
 df['MOM']=talib.MOM(np.array(close), timeperiod=5)
 df.tail(12)
+fig = plt.figure(figsize=[18,5])
+plt.plot(df.index,df['RSI'],label='RSI')
+plt.plot(df.index,df['MOM'],label='MOB')
+# plt.plot(df.index,mydif,label='my dif')
+# plt.plot(df.index,mydea,label='my dea')
+# plt.plot(df.index,mybar,label='my bar')
+plt.legend(loc='best')
+#plt.plot(df)
+plt.grid()
+#记得加这一句，不然不会显示图像
+plt.show()
--- macd.py
+++ macd.py
@@ -2,14 +2,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 import talib
 import tushare as ts
-df=ts.get_k_data('600600')
-close = [float(x) for x in df['close']]
-# 调用talib计算指数移动平均线的值
-df['EMA12'] = talib.EMA(np.array(close), timeperiod=6)  
-df['EMA26'] = talib.EMA(np.array(close), timeperiod=12)   
- # 调用talib计算MACD指标
-df['MACD'],df['MACDsignal'],df['MACDhist'] = talib.MACD(np.array(close),
-                            fastperiod=6, slowperiod=12, signalperiod=9)   
-print(df.tail(12))
-plt.plot(df)
-plt.show()
+
+
+
+
+
+
+def my_macd(price, fastperiod=12, slowperiod=26, signalperiod=9):
+    macdDIFF, macdDEA, macd = talib.MACDEXT(price, fastperiod=fastperiod, fastmatype=1, slowperiod=slowperiod, slowmatype=1, signalperiod=signalperiod, signalmatype=1)
+    macd = macd * 2
+    return macdDIFF, macdDEA, macd
+
+
+
+
+
+if __name__ == "__main__":
+    df=ts.get_k_data('600600',start='2018-01-01')
+    close = [float(x) for x in df['close']]
+    # 调用talib计算指数移动平均线的值
+    df['EMA12'] = talib.EMA(np.array(close), timeperiod=6)
+    df['EMA26'] = talib.EMA(np.array(close), timeperiod=12)
+     # 调用talib计算MACD指标
+    df['MACD'],df['MACDsignal'],df['MACDhist'] = talib.MACD(np.array(close),
+                                fastperiod=6, slowperiod=12, signalperiod=9)
+    df['my_MACD'],df['my_MACDsignal'],df['my_MACDhist'] = my_macd(df['close'].values,
+                                fastperiod=6, slowperiod=12, signalperiod=9)
+    print(df.tail(12))
+
+
+    fig = plt.figure(figsize=[18,5])
+    #plt.plot(df.index,df['MACD'],label='macd dif')
+    #plt.plot(df.index,df['MACDsignal'],label='signal dea')
+    plt.plot(df.index,df['MACDhist'] ,label='MACD')
+    #plt.plot(df.index,df['my_MACD'],label='my dif')
+    #plt.plot(df.index,df['my_MACDsignal'],label='my dea')
+    plt.plot(df.index,df['my_MACDhist'],label='my bar')
+    plt.legend(loc='best')
+    #plt.plot(df)
+    plt.grid()
+    #记得加这一句，不然不会显示图像
+    plt.show()
