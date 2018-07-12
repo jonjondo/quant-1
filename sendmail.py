61f64bdd8852bbe854574e511a0734bc4af379ce
wangpenghehe <wangpenghehe@gmail.com>
2018-07-12 08:29:59 +0800

    修改密送人的方式

--- sendmail.py
+++ sendmail.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import smtplib,time,getopt,sys  
 from email.mime.text import MIMEText  
-mailto_list=['wangpenghehe@qq.com','11861040@qq.com','3377499@qq.com','55695287@qq.com','zhoubinjason@gmail.com']
+#mailto_list=['wangpenghehe@qq.com','11861040@qq.com','3377499@qq.com','55695287@qq.com','zhoubinjason@gmail.com']
 mailbcc_list=['2838919549@qq.com']
-#mailto_list=['wangpenghehe@qq.com']
+mailcc_list=['']
+mailto_list=['wangpenghehe@qq.com']
 '''
 
 11861040@qq.com
@@ -53,12 +54,14 @@ def send_mail(to_list,sub,content):
     msg['Subject'] = sub  
     msg['From'] = me  
     msg['To'] = ";".join(to_list)  
-    msg['Bcc'] = ";".join(mailbcc_list)  
+    recieve = mailto_list
+    recieve.extend(mailcc_list)
+    recieve.extend(mailbcc_list) 
     try:  
         server = smtplib.SMTP_SSL()  
         server.connect(mail_host)  
         server.login(mail_user,mail_pass)  
-        server.sendmail(me, to_list, msg.as_string())  
+        server.sendmail(me, recieve, msg.as_string())  
         server.close()  
         return True  
     except Exception as e:
