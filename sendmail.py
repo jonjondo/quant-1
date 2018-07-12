46db48dd41a897be23915b0d83ca27e7d3c4a1b7
wangpenghehe <wangpenghehe@qq.com>
2018-07-12 08:04:16 +0800

    1.加入了密送的一些代码

--- sendmail.py
+++ sendmail.py
@@ -2,6 +2,7 @@
 import smtplib,time,getopt,sys  
 from email.mime.text import MIMEText  
 mailto_list=['wangpenghehe@qq.com','11861040@qq.com','3377499@qq.com','55695287@qq.com','zhoubinjason@gmail.com']
+mailbcc_list=['2838919549@qq.com']
 #mailto_list=['wangpenghehe@qq.com']
 '''
 
@@ -52,6 +53,7 @@ def send_mail(to_list,sub,content):
     msg['Subject'] = sub  
     msg['From'] = me  
     msg['To'] = ";".join(to_list)  
+    msg['Bcc'] = ";".join(mailbcc_list)  
     try:  
         server = smtplib.SMTP_SSL()  
         server.connect(mail_host)  
