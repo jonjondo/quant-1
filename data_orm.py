1f93e0a7296c64e951cfc7784048a6ec85b0538c
wangpenghehe <wangpenghehe@qq.com>
2018-07-18 08:55:34 +0800

    1.提交orm的类定义 2.提交发送微信模板消息的代码

---
+++ data_orm.py
@@ -0,0 +1,60 @@
+__author__ = 'lottiwang'
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, Index
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import create_engine
+Base = declarative_base()
+
+class User(Base):
+    # 表的名字
+    __tablename__ = 'wxuser'
+    # 表的结构
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    useropenid = Column(String(28))
+    name = Column(String(20))
+    remark = Column(String(20))
+    groupid = Column(String(20))
+    subscribe  = Column(Integer)
+
+    __table_args__ = (
+    UniqueConstraint('id', 'useropenid', name='uix_id_useropenid'),
+    Index('ix_id_useropenid', 'useropenid', 'name'),
+)
+
+
+class Stock(Base):
+    # 表的名字
+    __tablename__ = 'stock'
+    # 表的结构
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    stockcode = Column(String(10))
+    stockname = Column(String(25))
+    industyid = Column(String(20))
+    marketid = Column(String(20))
+    operation  = Column(Integer)
+
+    __table_args__ = (
+    UniqueConstraint('id', 'stockcode', name='uix_id_stockcode'),
+    Index('ix_id_stockcode', 'stockcode', 'stockname'),
+)
+
+
+class StockRecord(Base):
+    # 表的名字
+    __tablename__ = 'stockrecord'
+    # 表的结构
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    stockid = Column(Integer,ForeignKey('stock.id'))
+    userid = Column(Integer,ForeignKey('wxuser.id'))
+    operation  = Column(Integer)
+    recordtime = Column(Integer)
+
+    __table_args__ = (
+    UniqueConstraint('id', 'stockid', name='uix_id_stockid'),
+    Index('ix_id_stockid', 'stockid', 'stockid','userid'),
+)
+
+
+engine = create_engine('mysql+pymysql://root:@localhost:3306/quant')
+Base.metadata.create_all(engine)
---
+++ wechat_assit.py
@@ -0,0 +1,86 @@
+__author__ = 'lottiwang'
+from wechatpy import WeChatClient
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, Index
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import create_engine
+from data_orm import *
+import time
+APPID = "wx8e5ae8389bb42ffe"
+APPSECRET = "772cba9ec8991bc97b145a240c9f48af"
+session = None
+
+
+
+def update_wechat_userinfo(client,session):
+    user = client.user.get_followers()
+    print(user)
+
+    user_info = client.user.get_batch(user['data']['openid'])
+    print(user_info)
+    for info in user_info:
+        user = session.query(User).filter(User.useropenid == info['openid']).first()
+        if user == None:
+            u=User(useropenid=info['openid'],name=info['nickname'],remark=info['remark'],groupid=info['groupid'],subscribe=info['subscribe'])
+            session.add(u)
+    session.commit()
+
+
+def search_user_by_name(username):
+    # 创建Query查询, filter是where条件, 最后调用one()返回唯一行, 如果调用all()则返回所有行.
+    user = session.query(User).filter(User.name==username).one()
+    print('name:', user.name)
+    print('openid:', user.useropenid)
+    session.close()
+
+def send_template_msg(userid,stockname,stockid,price,operation):
+    data={}
+    if operation == 'SELL':
+        caption = '您关注的股票触发卖出提醒'
+        opcolor = "#00FF00"
+        tmpid = 'MHyi8np240SK4TD5tLqWPhna9Bg2Xdh9dUUsJAouqsc'
+    else:
+        caption = '您关注的股票触发买入提醒'
+        opcolor = "#FF0000"
+        tmpid = '2ab6uPK6RdLotx4599G-Cpo3Br_yrNuUeUf9tzovxE0'
+
+    data['first']={'value':caption,'color':opcolor}
+    data['keyword1']={'value': stockname,'color':"#173177"}
+    data['keyword2']={'value': stockid,'color':"#173177"}
+    data['keyword3']={'value': operation,'color':opcolor}
+    data['keyword4']={'value': price,'color':"#173177"}
+    data['keyword5']={'value': time.strftime("%Y%m%d %H:%M",time.localtime(time.time()))}
+    data['remark']={'value':'以上信息只提示一次，不会反复，建议仅供参考','color':"#FF0000"}
+    '''
+    {{first.DATA}}
+    股票名称：{{keyword1.DATA}}
+    股票代码：{{keyword2.DATA}}
+    操作类型：{{keyword3.DATA}}
+    卖出价格：{{keyword4.DATA}}
+    交易时间：{{keyword5.DATA}}
+    {{remark.DATA}}
+    '''
+
+    client.message.send_template(userid,tmpid,data,None,None)
+
+if __name__ == "__main__":
+    client = WeChatClient('wx8e5ae8389bb42ffe', '772cba9ec8991bc97b145a240c9f48af')
+    # 初始化数据库连接
+    engine = create_engine('mysql+pymysql://root:@localhost:3306/quant')
+    # 创建DBSession类型
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    #update_wechat_userinfo(client,session)
+    #search_user_by_name('==')
+    send_template_msg('o48rB0qa3htdR_RzzG7seKSuEPsY','腾讯控股','HK.00700',420,'SELL')
+    send_template_msg('o48rB0qa3htdR_RzzG7seKSuEPsY','新城发展控股','HK.01030',6.5,'BUY')
+
+
+
+
+
+
+
+
+
+
