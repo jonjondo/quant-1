9aa81d6d72f7952e708b484ca35d86d57e739ae7
wangpenghehe <wangpenghehe@gmail.com>
2018-10-13 13:51:26 +0800

    添加发附件的功能

--- sendmail.py
+++ sendmail.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import smtplib,time,getopt,sys  
 from email.mime.text import MIMEText  
+from email.mime.multipart import MIMEMultipart
 mailto_list=['wangpenghehe@qq.com','11861040@qq.com','3377499@qq.com','55695287@qq.com','zhoubinjason@gmail.com']
 mailbcc_list=['2838919549@qq.com','langzm@qq.com','ruby@fygame.com']
 mailcc_list=['']
@@ -15,7 +16,7 @@ zhoubinjason@gmail.com
 '''
 mail_host="smtp.qq.com:465" 
 mail_user="wangpenghehe"  
-mail_pass="tingting520"   
+mail_pass="uwxypbotigynbgbb"   
 mail_postfix="qq.com"  
 global message
 global outfilename
@@ -58,7 +59,8 @@ def send_mail(to_list,sub,content):
     recieve.extend(mailcc_list)
     recieve.extend(mailbcc_list) 
     try:  
-        server = smtplib.SMTP_SSL()  
+        server = smtplib.SMTP_SSL() 
+        server.set_debuglevel(1)
         server.connect(mail_host)  
         server.login(mail_user,mail_pass)  
         server.sendmail(me, recieve, msg.as_string())  
@@ -68,6 +70,33 @@ def send_mail(to_list,sub,content):
         print(str(e)  )
         return False
         
+def send_mail_with_attach(sub,content,file_full_path):
+    me="WGQuant Notification"+"<"+mail_user+"@"+mail_postfix+">"
+    msg = MIMEMultipart() 
+    msg['Subject'] = sub  
+    msg['From'] = me  
+    msg['To'] = ";".join(mailto_list) 
+    msg.attach(MIMEText(content, 'html', 'utf-8'))
+    # 构造附件
+    att = MIMEText(open(file_full_path, "rb").read(), "base64", "utf-8")
+    att["Content-Type"] = "application/octet-stream"
+    #附件名称为中文时的写法
+    att.add_header("Content-Disposition", "attachment", filename=("gbk", "", file_full_path.split('/')[-1]))
+    msg.attach(att)
+    recieve = mailto_list
+    recieve.extend(mailcc_list)
+    recieve.extend(mailbcc_list) 
+    try:  
+        server = smtplib.SMTP_SSL() 
+        server.set_debuglevel(1)
+        server.connect(mail_host)  
+        server.login(mail_user,mail_pass)  
+        server.sendmail(me, recieve, msg.as_string())  
+        server.close()  
+        return True  
+    except Exception as e:
+        print(str(e)  )
+        return False
 def send_mail_to_me(to_list,sub,content):
     me="WGQuant Notification"+"<"+mail_user+"@"+mail_postfix+">"
     msg = MIMEText(content,_subtype='html',_charset='utf-8')
